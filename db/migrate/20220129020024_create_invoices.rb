3974e12dcf19e5456116796507c83d4621a5945d
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-01-28 23:04:40 -0300

    migrate invoice model

--- db/migrate/20220129020024_create_invoices.rb
+++ db/migrate/20220129020024_create_invoices.rb
@@ -1,7 +1,14 @@
 class CreateInvoices < ActiveRecord::Migration[6.0]
   def change
     create_table :invoices do |t|
-
+      t.string :name
+      t.integer :value
+      t.string :code
+      t.boolean :paid, default: false
+      t.string :description
+      t.string :address
+      t.date :invoice_date
+      t.date :payment_date
       t.timestamps
     end
   end
