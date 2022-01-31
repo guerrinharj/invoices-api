30ae3cea37d7e496310ba35ab70cccc99ada1b18
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-01-31 11:37:54 -0300

    did update endpoint

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -6,10 +6,21 @@ class Api::V1::InvoicesController < Api::V1::BaseController
   end
 
   def show
+  end
 
+  def update
+    if @invoice.update(invoice_params)
+      render :show
+    else
+      render_error
+    end
   end
 
   private
+
+  def invoice_params
+    params.require(:invoice).permit(:name, :email, :value, :code, :invoice_date, :payment_date, :paid, :client_address, :sender_address, :items, :description, :terms)
+  end
 
   def set_invoice
     @invoice = Invoice.find(params[:id])
