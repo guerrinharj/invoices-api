257abbf83774d271cc868bceb3c9a84e7d04cb75
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-01-31 11:19:51 -0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :restaurants, only: [ :index, :show, :create, :edit, :destroy ]
+    end
+  end
 end
