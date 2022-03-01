7988bf1745b9706e1b8ca9495df80322b745591d
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-03-01 05:52:02 -0300

    installed devise and added reference

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,7 @@
 class Invoice < ApplicationRecord
   serialize :client_address
   serialize :sender_address
-
   validates :email, uniqueness: true
+
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
+    devise_for :users
     namespace :v1 do
       resources :invoices, only: [:index, :show, :create, :edit, :destroy]
     end
