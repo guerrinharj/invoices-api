92eddd6d9a377a1832f55e9609847159c793a0da
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-03-03 17:05:13 -0300

    generate code

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,9 @@
+require 'faker'
+
 class Invoice < ApplicationRecord
   belongs_to :user
   after_initialize :random_user
+  after_initiliaze :generate_code
 
   serialize :client_address
   serialize :sender_address
@@ -9,5 +12,9 @@ class Invoice < ApplicationRecord
 
   def random_user
     self.user = User.order('RANDOM()').first if self.user.nil?
+  end
+
+  def generate_code
+    self.code = Faker::Code.nric if self.code.nil?
   end
 end
